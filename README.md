2f3377d5b7c3b5cbfe87bcf560fcea0a91921aee
RicardoEGFST <a20216256@pucp.edu.pe>
2024-09-07 15:44:01 -0500

    Avance de SuperAdmin

--- README.md
+++
@@ -1,2 +0,0 @@
-# GTICS_HTML
-En las carpetas de los roles administrativos esta un archivo con una base que pueden usar como referencia
--- assets/js/vendors/modal.js
+++
@@ -1,1 +0,0 @@
-window.addEventListener("load",function(){setTimeout(function(){new bootstrap.Modal(document.getElementById("modal-subscribe")).show()},3e3)});
